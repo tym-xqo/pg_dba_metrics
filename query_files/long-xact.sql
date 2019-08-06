179289758fc883c69f1edc480b0f4847e7678be3
PostgreSQL Server <postgres@db02.benchprep.com>
2019-08-06 15:58:48 -0500

    add long-xact query

---
+++ query_files/long-xact.sql
@@ -0,0 +1,20 @@
+---
+status: clear
+threshold:
+  field: duration
+  gate: 300
+---
+WITH i AS (
+SELECT pid
+     , usename
+     , left(query, 80) AS query
+     , extract(epoch FROM age(clock_timestamp(), xact_start)) AS duration
+ FROM pg_stat_activity
+WHERE backend_type = 'client backend'
+  AND state = 'idle in transaction'
+ORDER BY 4 desc
+LIMIT 1)
+SELECT * FROM i 
+UNION
+SELECT 0, 'no user', 'no query', 0
+ WHERE NOT EXISTS (SELECT * FROM i)
