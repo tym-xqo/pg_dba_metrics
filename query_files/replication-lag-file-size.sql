6c1bc1a74137a45357dff52977f6ec04deea665d
Thomas Yager-Madden <10351894+tym-xqo@users.noreply.github.com>
2019-10-25 13:28:31 -0500

    Add empty-row defaults to replication check queries

--- query_files/replication-lag-file-size.sql
+++ query_files/replication-lag-file-size.sql
@@ -4,8 +4,15 @@ threshold:
   field: restart_lag
   gate: 33554432
 ---
+with lag as (
 SELECT
   pg_wal_lsn_diff(pg_current_wal_lsn(), restart_lsn)::INTEGER AS restart_lag,
   pg_wal_lsn_diff(pg_current_wal_lsn(), confirmed_flush_lsn)::INTEGER AS flush_lag
 FROM pg_replication_slots
-WHERE slot_name = 'session_database_replication_slot';
+WHERE slot_name = 'session_database_replication_slot'
+)
+SELECT * FROM lag
+UNION
+SELECT 0, 0 
+WHERE NOT EXISTS (SELECT * FROM lag)
+;
