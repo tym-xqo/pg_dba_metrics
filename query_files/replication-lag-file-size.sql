47b9ab53392429f3eb9ae62df7ea796a0cf41279
Thomas Yager-Madden <10351894+tym-xqo@users.noreply.github.com>
2019-10-29 13:41:43 -0500

    Pretty the lag for display

--- query_files/replication-lag-file-size.sql
+++ query_files/replication-lag-file-size.sql
@@ -8,11 +8,12 @@ with lag as (
 SELECT
   slot_name,
   pg_wal_lsn_diff(pg_current_wal_lsn(), restart_lsn)::INTEGER AS restart_lag,
+  pg_size_pretty(pg_wal_lsn_diff(pg_current_wal_lsn(), restart_lsn)) as pretty_restart_lag,
   pg_wal_lsn_diff(pg_current_wal_lsn(), confirmed_flush_lsn)::INTEGER AS flush_lag
 FROM pg_replication_slots
 )
 SELECT * FROM lag
 UNION
-SELECT 0, 0 
+SELECT NULL, 0, '', 0 
 WHERE NOT EXISTS (SELECT * FROM lag)
 ;
--- query_files/replication-last-receipt-msg.sql
+++ query_files/replication-last-receipt-msg.sql
@@ -11,6 +11,6 @@ EXTRACT(EPOCH FROM AGE(NOW(), last_msg_receipt_time)) AS duration
 FROM pg_stat_subscription)
 SELECT * FROM submsg
 UNION
-SELECT 0
+SELECT NULL, 0
 WHERE NOT EXISTS (SELECT * FROM submsg)
 ;
