9c545ea0cbac3799c8bb3ec764dcb494661e121e
Thomas Yager-Madden <10351894+tym-xqo@users.noreply.github.com>
2019-10-29 13:26:38 -0500

    check lag on all replication slots and subscriptions

--- query_files/replication-lag-file-size.sql
+++ query_files/replication-lag-file-size.sql
@@ -6,10 +6,10 @@ threshold:
 ---
 with lag as (
 SELECT
+  slot_name,
   pg_wal_lsn_diff(pg_current_wal_lsn(), restart_lsn)::INTEGER AS restart_lag,
   pg_wal_lsn_diff(pg_current_wal_lsn(), confirmed_flush_lsn)::INTEGER AS flush_lag
 FROM pg_replication_slots
-WHERE slot_name = 'session_database_replication_slot'
 )
 SELECT * FROM lag
 UNION
--- query_files/replication-last-receipt-msg.sql
+++ query_files/replication-last-receipt-msg.sql
@@ -5,7 +5,9 @@ threshold:
   gate: 300
 ---
 with submsg as (
-SELECT EXTRACT(EPOCH FROM AGE(NOW(), last_msg_receipt_time)) AS duration
+SELECT 
+subname as subscription_name,
+EXTRACT(EPOCH FROM AGE(NOW(), last_msg_receipt_time)) AS duration
 FROM pg_stat_subscription)
 SELECT * FROM submsg
 UNION
