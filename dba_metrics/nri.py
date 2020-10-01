9421976c6542f092611e94b93d7a18ed7f12a5c0
Thomas Yager-Madden <10351894+tym-xqo@users.noreply.github.com>
2020-10-01 11:21:12 -0500

    Improve naming in payload

--- dba_metrics/nri.py
+++ dba_metrics/nri.py
@@ -1,6 +1,9 @@
 import json
 
 from dba_metrics.__main__ import all_metrics, get_metric
+from dotenv import find_dotenv, load_dotenv
+
+load_dotenv(find_dotenv(), override=True)
 
 JSON_HEADER = """
 {
@@ -10,8 +13,8 @@ JSON_HEADER = """
   "data": [
     {
       "entity": {
-        "name": "test_entity",
-        "type": "meaningless_number",
+        "name": "postgres-custom",
+        "type": "database-metric",
         "id_attributes": [
           {
             "key": "environment",
@@ -21,9 +24,38 @@ JSON_HEADER = """
       },
       "metrics": [{}],
       "inventory": {},
-      "events": []
+      "events": [],
+      "add_hostname": true
     }
   ]
+}
+"""
+
+JSON_BLOCK = """
+{
+  "name": "com.benchprep.test-integration",
+  "protocol_version": "3",
+  "integration_version": "0.0.1",
+  "data": [{
+    "entity": {
+      "name": "postgres-custom",
+      "type": "database-metric",
+      "id_attributes": [{
+        "key": "environment",
+        "value": "development"
+      }]
+    },
+    "metrics": [{
+      "displayName": "postgres-custom",
+      "entityName": "database-metric:postgres-custom",
+      "event_type": "BenchPrepTestEvent",
+      "bppg.avg-active.avg_duration": 0.001298,
+      "bppg.locks.lock_count": 1
+    }],
+    "inventory": {},
+    "events": [],
+    "add_hostname": true
+  }]
 }
 """
 
@@ -37,7 +69,7 @@ def sample_metrics():
     metrics = dict(
         displayName="test_entity",
         entityName="meaningless_number:test_entity",
-        event_type="TestEvent",
+        event_type="BenchPrepTestEvent",
     )
     for metric in all_metrics():
         metric = get_metric(metric)
@@ -46,17 +78,20 @@ def sample_metrics():
         data = metric.result
         check = metric.metadata["threshold"]["field"]
         value = max([row[check] for row in data])
-        metric_key = ".".join([metric.name, check])
+        metric_key = ".".join(["bppg", metric.name, check])
         metrics[metric_key] = value
     metrics = [metrics]
     return metrics
 
 
 def main():
-    header = setup_header()
-    metrics = sample_metrics()
-    header["metrics"] = metrics
-    payload = json.dumps(header)
+    # header = setup_header()
+    # metrics = sample_metrics()
+    # header["data"][0]["metrics"] = metrics
+    # payload = json.dumps(header)
+    # print(payload)
+    block = json.loads(JSON_BLOCK)
+    payload = json.dumps(block)
     print(payload)
 
 
