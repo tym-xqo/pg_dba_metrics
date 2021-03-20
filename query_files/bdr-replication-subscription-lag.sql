e96c81714719040ad49a1856eacff75b24c3bb69
Thomas Yager-Madden <10351894+tym-xqo@users.noreply.github.com>
2021-03-19 20:09:24 -0500

    Handle empty bdr subscriptions

--- query_files/bdr-replication-subscription-lag.sql
+++ query_files/bdr-replication-subscription-lag.sql
@@ -5,10 +5,21 @@ threshold:
   gate: 300
 ---
 */ 
+with s as (
 select node_group_name 
      , origin_name 
      , sub_enabled 
      , sub_slot_name 
      , subscription_status 
      , extract(epoch from age(now(), last_xact_replay_timestamp)) as time_lag
-  from bdr.subscription_summary;
+  from bdr.subscription_summary
+)
+select * from s
+union
+select 'wmx_rails_api'
+     , 'na'
+     , false
+     , 'na'
+     , 'na'
+     , 0
+ where not exists (select * from s); 
